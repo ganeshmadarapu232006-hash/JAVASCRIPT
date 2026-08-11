f02104acd8f780a8ee5df68dc2557e13c1f47cba
Ganga Manohar <ganeshmadarapu232006@gmail.com>
2026-08-11 14:57:08 +0530

    add javascript practise

---
+++ f11.js
@@ -0,0 +1,5 @@
+function add (a,b) {
+    return a + b;
+}
+let x = add(10,60);
+console.log(x);
--- f8.js
+++ f8.js
@@ -2,4 +2,4 @@ function test() {
     return(15)
     console.log("Hello");
 }
- console.log(test());
+ console.log(test()); 
